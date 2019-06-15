b925719873252b69457b2002ba9f6c1c1fe2b9e6
Nic Gibson <nic@shpangy.com>
2019-06-15 10:35:09 -0600

    Initial Commit

---
+++ source_control_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  source_control_2
+//
+//  Created by Nic Gibson on 6/15/19.
+//  Copyright © 2019 Nic Gibson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
